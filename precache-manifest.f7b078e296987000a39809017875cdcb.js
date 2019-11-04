019484f24f0a4f115871d6b804883b03a4167f3c
Javad <mirjavad@webtamin.com>
2019-11-04 19:52:00 +0330

    Updates

---
+++ precache-manifest.f7b078e296987000a39809017875cdcb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9098e438ef0c56bd018cf7f1aa8005a",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "e7701d2406e91e4ae82a",
+    "url": "/calculator/static/css/main.b2747804.chunk.css"
+  },
+  {
+    "revision": "8e82e41a9304022d255c",
+    "url": "/calculator/static/js/2.1f0b9671.chunk.js"
+  },
+  {
+    "revision": "e7701d2406e91e4ae82a",
+    "url": "/calculator/static/js/main.43a093a0.chunk.js"
+  },
+  {
+    "revision": "cc9df0c46115d4c41a58",
+    "url": "/calculator/static/js/runtime~main.a2fafb2c.js"
+  }
+]);
